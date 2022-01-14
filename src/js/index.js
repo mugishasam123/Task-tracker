62f0a2b5cbecbc049ac2b7f20b2879f625ac4085
mugisha Samuel <mugishasamuel42@gmail.com>
2022-01-14 05:53:58 -0800

    ADD remove and add methods

--- src/js/index.js
+++ src/js/index.js
@@ -21,14 +21,17 @@ const cleardom = () => {
 const rerender = (tasks) => {
   tasks.forEach((value) => {
     const data = `
-              <p class="shared-property task">
-                <input
+    <div class = "goesto">
+    <input
                   type="checkbox"
                   name=""
                   id=""
       
-                />&nbsp;${value.description}
+                />
+              <p  contenteditable="false" class="shared-property editpara">
+                &nbsp;${value.description}
               </p>
+              </div>
               <i class="hover-delete fa fa-ellipsis-v"></i>
               <i class="delete-icon fa fa-trash-alt"></i>
             `;
@@ -39,6 +42,8 @@ const rerender = (tasks) => {
     listnode.innerHTML = data;
     taskt.appendChild(listnode);
   });
+  const editable = document.querySelectorAll('.task');
+  const paraedit = document.querySelectorAll('.editpara');
 
   const deletetask = (indexer) => {
     tasks.splice(indexer, 1);
@@ -55,12 +60,42 @@ const rerender = (tasks) => {
   const hoverdelete = document.querySelectorAll('.hover-delete');
   const deletebtn = document.querySelectorAll('.delete-icon');
 
+  const updatetask = (newdescription, atindex) => {
+    tasks[atindex].description = newdescription;
+    localStorage.setItem('taskdata', JSON.stringify(tasks));
+    cleardom();
+    rerender(tasks);
+  };
+
   hoverdelete.forEach((dete, indexdate) => {
     dete.addEventListener('click', (e) => {
       e.target.style.display = 'none';
       deletebtn.forEach((deleti, indexdata) => {
         if (indexdate === indexdata) {
           deletebtn[indexdata].style.display = 'flex';
+        }
+      });
+      editable.forEach((editem, editindex) => {
+        if (editindex === indexdate) {
+          editable[editindex].style.background = '#FFF0CF';
+
+          paraedit.forEach((paratem, parindex) => {
+            paratem.addEventListener('keyup', (e) => {
+              if (parindex === editindex) {
+                paratem.setAttribute('contenteditable', 'true');
+                if (e.keyCode === 13) {
+                  paratem.setAttribute('contenteditable', 'false');
+                  editable[editindex].style.background = 'white';
+
+                  updatetask(paratem.textContent, parindex);
+                }
+              }
+            });
+
+            if (parindex === editindex) {
+              paraedit[parindex].setAttribute('contenteditable', 'true');
+            }
+          });
         }
       });
     });
